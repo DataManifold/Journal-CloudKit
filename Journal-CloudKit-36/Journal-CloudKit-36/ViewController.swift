5856cb75cae5e80f0caceb1709f0e9c45e9d4308
Shean Bjoralt <shean@datamanifold.com>
2020-10-05 14:22:12 -0600

    initial commit

---
+++ Journal-CloudKit-36/Journal-CloudKit-36/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal-CloudKit-36
+//
+//  Created by Shean Bjoralt on 10/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
